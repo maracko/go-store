4d4c9ebbf5aedb80bd04a9bd8c441d9298a8926d
Mario Petričko <petricko.mario@gmail.com>
2022-01-10 08:35:50 +0100

    add viper support

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,16 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	DEFAULT_CONFIG_FILENAME = ".gostore-config"
+	ENV_PREFIX              = "GOSTORE"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -14,6 +22,9 @@ var rootCmd = &cobra.Command{
 	Long: `Go store creates an in memory database similar to redis.
 	You can start TCP or HTTP server and access them with a CLI or HTTP requests.
 	After server shutdown a json encoded file with your data will be written.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return initializeConfig(cmd)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -25,6 +36,55 @@ func Execute() {
 	}
 }
 
-func init() {
+func initializeConfig(cmd *cobra.Command) error {
+	v := viper.New()
 
+	// Set the base name of the config file, without the file extension.
+	v.SetConfigName(DEFAULT_CONFIG_FILENAME)
+
+	// Set as many paths as you like where viper should look for the config file
+	v.AddConfigPath(".")
+
+	// Attempt to read the config file, gracefully ignoring errors
+	// caused by a config file not being found
+	if err := v.ReadInConfig(); err != nil {
+		// It's okay if there isn't a config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	// When we bind flags to environment variables expect that the
+	// environment variables are prefixed, e.g. a flag like --number
+	// binds to an environment variable STING_NUMBER. This helps
+	// avoid conflicts.
+	v.SetEnvPrefix(ENV_PREFIX)
+
+	// Bind to environment variables
+	// Works great for simple config names, but needs help for names
+	// like --favorite-color which we fix in the bindFlags function
+	v.AutomaticEnv()
+
+	// Bind the current command's flags to viper
+	bindFlags(cmd, v)
+
+	return nil
+}
+
+// Bind each cobra flag to its associated viper configuration (config file and environment variable)
+func bindFlags(cmd *cobra.Command, v *viper.Viper) {
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		// Environment variables can't have dashes in them, so bind them to their equivalent
+		// keys with underscores, e.g. --this-flag becomes GOSTORE_THIS_FLAG
+		if strings.Contains(f.Name, "-") {
+			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
+			_ = v.BindEnv(f.Name, fmt.Sprintf("%s_%s", ENV_PREFIX, envVarSuffix))
+		}
+
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if !f.Changed && v.IsSet(f.Name) {
+			val := v.Get(f.Name)
+			_ = cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v1.1.3
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.10.1 // indirect
 )
